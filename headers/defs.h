2d993dfcd8a880649dfb8ee6033e44fcd65f2483
Mochida <mochida@42>
2022-08-27 22:18:56 +0200

    feat(get_env): done, also has a function to destroy the whole list

--- headers/defs.h
+++ headers/defs.h
@@ -1,4 +1,10 @@
 #ifndef DEF_H
 # define DEF_H
 
+typedef struct	s_env_list
+{
+	char	*content;
+	struct s_env_list *next;
+}	t_env_list;
+
 #endif
---
+++ src/get_env.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_env.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 21:38:27 by coder             #+#    #+#             */
+/*   Updated: 2022/08/27 22:17:50 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minishell.h"
+
+/*
+** Gets all eviromental variables and saves them in a list pointer;
+** All things are allocated;
+** It's the caller responsability do destroy the list after use;
+*/
+t_env_list	*get_env(char **envp)
+{
+	int	index;
+	t_env_list *self;
+	t_env_list	*temp;
+
+	index = 0;
+	self = ft_calloc(1, sizeof(*self));
+	temp = self;
+	while (envp[index])
+	{
+		temp->content = ft_strdup(envp[index]);
+		temp->next = ft_calloc(1, sizeof(*self));
+		temp = temp->next;
+		index++;
+	}
+	safe_free (temp->next);
+	return (self);
+}
+
+/*
+** Destroys a MFing list;
+** Also nullifies every content inside, because why not?
+*/
+void	destroy_env_list(t_env_list *envs)
+{
+	t_env_list *temp;
+	t_env_list *freeme;
+
+	temp = envs;
+	while (temp)
+	{
+		freeme = temp;
+		temp = temp->next;
+		safe_free (freeme->content);
+		safe_free (freeme);
+	}
+}
