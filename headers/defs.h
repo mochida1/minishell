c05af72c089df424bfe01ce8e566a4b0901def9c
Mochida <mochida@42>
2022-08-28 03:25:08 +0200

    make git

--- headers/defs.h
+++ headers/defs.h
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 23:09:13 by coder             #+#    #+#             */
-/*   Updated: 2022/08/27 23:11:32 by coder            ###   ########.fr       */
+/*   Updated: 2022/08/28 02:52:22 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,14 @@ typedef struct s_env_list
 	char				*content;
 	struct s_env_list	*next;
 }				t_env_list;
+
+typedef struct s_ms_data
+{
+	int	state;
+	char	*rl_buffer;
+	char	*rl_prompt;
+	struct s_env_list *env_head;
+
+}				t_ms_data;
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,19 +6,75 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 01:58:25 by viferrei          #+#    #+#             */
-/*   Updated: 2022/08/27 23:00:34 by coder            ###   ########.fr       */
+/*   Updated: 2022/08/28 03:23:34 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-int	main(void)
+int set_prompt(t_ms_data *ms)
 {
-	char	*buffer;
+	ms->rl_prompt = "\360\237\246\204 :)";
+	return (0);
+}
 
-	while (1)
+int	get_data_from_readline(t_ms_data *ms)
+{
+	safe_free(ms->rl_buffer);
+	ms->rl_buffer = readline(ms->rl_prompt);
+	if (ms->rl_buffer)
+		return (0);
+	else if (!(ms->rl_buffer))
+		return (1);
+	else if (ms->rl_buffer < 0)
+		return (-1);
+	return (0);
+}
+
+int	update_data (t_ms_data *ms, char **envp)
+{
+	safe_free (ms->env_head);
+	ms->env_head = get_env(envp);
+	if (ms->env_head)
+		return (0);
+	ms->state++;
+	return (1);
+}
+
+static int shutup_unused_args(int delete, char me)
+{
+	return (delete + me);
+}
+
+int	main(int argc, char *argv[], char **envp)
+{
+	t_ms_data *ms;
+	int	rc;
+
+	ms = ft_calloc (1, sizeof(*ms));
+	rc = 0;
+	ms->state = 0;
+	while (!rc)
 	{
-		buffer = readline("$> ");
+		if (ms->state < 0)
+			exit(ms->state * -1);
+		if (ms->state == 0)
+		{
+			rc = update_data(ms, envp);
+			rc = shutup_unused_args(argc, argv[0][0]);
+		}
+		if (ms->state == 1)
+		{
+			rc = set_prompt(ms);
+			rc = get_data_from_readline(ms);
+			if (rc)
+				ms->state = 2;
+		}
+		if (ms->state == 2)
+		{
+
+		}
+
 	}
-	free(buffer);
+	return (0);
 }
