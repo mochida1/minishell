e11c27e757809dd53e323c5c59745c6df87b6743
viferrei <vifm@outlook.com>
2022-08-30 17:32:48 -0300

    test Makefile updated with only needed src files

--- tests/files/main_tests.c
+++ tests/files/main_tests.c
@@ -6,11 +6,11 @@
 /*   By: viferrei <viferrei@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 23:31:47 by viferrei          #+#    #+#             */
-/*   Updated: 2022/08/29 18:26:56 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/08/30 17:21:21 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../headers/minishell.h"
+#include "../../headers/minishell.h"
 
 void	test_get_names_size()
 {
