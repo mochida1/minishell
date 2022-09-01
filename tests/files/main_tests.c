778c6686dfd755df1ce2d6f029840e7271aa95a2
viferrei <vifm@outlook.com>
2022-08-31 21:12:37 -0300

    update for vscode debugger

--- tests/files/main_tests.c
+++ tests/files/main_tests.c
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 23:31:47 by viferrei          #+#    #+#             */
-/*   Updated: 2022/08/30 17:55:32 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/08/31 21:11:28 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,7 @@ void	test_get_names_size()
 	printf("var names size sum: %zu\n", get_names_size(str));
 }
 
-void	test_add_spaces()
-{
-	char	*quote_redirect = "<< hi this is \" a test<<t\'o>>test\" >><\'< \">\'>|>> \'>>\'<<>>";
-
-	printf("str: %s\n", quote_redirect);
-	printf("add_spaces: %s\n", add_spaces(quote_redirect));
-}
-
 int	main(void)
 {
-	// test_get_names_size();
-	test_add_spaces();
+	test_get_names_size();
 }
