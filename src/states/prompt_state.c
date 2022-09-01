57dc220f785360538d9a9b80d23e9f3dbf07f56b
Mochida <mochida@42>
2022-09-02 01:15:11 +0200

    feat(add_history): first test

--- src/states/prompt_state.c
+++ src/states/prompt_state.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 17:24:20 by coder             #+#    #+#             */
-/*   Updated: 2022/08/29 00:34:43 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/02 01:14:41 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	prompt_state(t_ms_data *ms)
 	int	rc;
 
 	rc = get_data_from_readline(ms);
+	if (ms->prompt_buffer && ms->prompt_buffer[0])
+		add_history (ms->prompt_buffer);
 	assert(rc == 0);
 	if (!rc)
 	{
