704277710c287ac33c76956a38d9165b4a4a2214
viferrei <vifm@outlook.com>
2022-09-06 23:53:53 +0200

    update_tokens, copy_value

--- src/states/expand_vars.c
+++ src/states/expand_vars.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand_vars.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: viferrei <viferrei@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 17:40:14 by viferrei          #+#    #+#             */
-/*   Updated: 2022/09/06 01:03:38 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/09/06 23:48:28 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,21 +102,70 @@ size_t	get_values_size(char *str, t_env_list *env)
 	return (size);
 }
 
-void	update_tokens(t_tokens *token, size_t final_size)
+char	*get_var_value(char *var_name, t_env_list *env)
+{
+	size_t	var_size;
+	size_t	len;
+	char	*str;
+	char	*var_value;
+
+	var_size = ft_strlen(var_name);
+	len = 0;
+	while (env->content)
+	{
+		str = env->content;
+		if(ft_strnstr(str, var_name, var_size) && *(str + var_size) == '=')
+		{
+			while (*(str - 1) != '=')
+				str++;
+			while(str[len])
+				len++;
+			var_value = ft_substr(str, 0, len);
+		}
+		env = env->next;
+	}
+	return (var_value);
+}
+
+void	copy_value(char *str, const char *value_name)
+{
+	int	i;
+
+	i = 0;
+	while (value_name[i])
+		str[i] = value_name[i];
+}
+
+void	update_tokens(t_tokens *token, size_t final_size, t_env_list *env)
 {
 	char	*old_value;
 	char	*var_name;
+	char	*value_name;
+	int		t;
+	int		o;
 
 	old_value = token->value;
 	token->value = (char *) malloc(sizeof(char) * final_size);
-	while (*old_value)
+	t = 0;
+	o = 0;
+	while (old_value[o])
 	{
-		if (*old_value == '$')
+		while (old_value[o] && (old_value[o] != '$'))
+			token->value[t++] = old_value[o++];
+		if (old_value[o] == '$')
 		{
 			var_name = get_var_name(old_value);
+			value_name = get_var_value(var_name, env);
 
+			copy_value(&token->value[t], value_name);
+			free(var_name);
+			free(value_name);
+			// while (!ft_isspace(old_value[o]) && !ft_isquote(old_value[o])
+			// 		&& old_value[o])
+			// 	o++;
 		}
-		old_value++;
+		if (old_value[o])
+			o++;
 	}
 	free(old_value);
 }
@@ -143,7 +192,7 @@ int	expand_variables(t_ms_data *ms)
 		names_size = get_names_size(ms->tokens->value);
 		values_size = get_values_size(ms->tokens->value, ms->env_head);
 		final_size = str_size - names_size + values_size + 1;
-		// update_tokens(ms->tokens, final_size);
+		update_tokens(ms->tokens, final_size, ms->env_head);
 		test_expand_vars(ms->tokens->value, str_size, names_size, values_size);
 		ms->tokens = ms->tokens->next;
 	}
