54561e21393bed4fafc208f2735108e0d9071603
viferrei <vifm@outlook.com>
2022-09-08 01:00:50 +0200

    comecei a expand_variables dividindo a string em dois. Parte um alocada

--- src/states/expand_vars.c
+++ src/states/expand_vars.c
@@ -6,195 +6,50 @@
 /*   By: viferrei <viferrei@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 17:40:14 by viferrei          #+#    #+#             */
-/*   Updated: 2022/09/07 22:18:11 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/09/08 00:59:36 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-// Iterates through string and sums the size of all variables started by "$"
-size_t	get_names_size(char *str)
+// Checks if the char is acceptable in a variable name.
+int	is_variable(char c)
 {
-	size_t	size;
+	if (ft_isalnum(c) || c == '_')
+		return (1);
+	return (0);
+}
 
-	size = 0;
+// Returns a pointer to the first occurence of a variable in the string.
+char	*find_variable(char	*str)
+{
 	while (*str)
 	{
-		if (*str == '$')
-		{
-			size++;
-			str++;
-			while (!ft_isspace(*str) && !ft_isquote(*str) && *str)
-			{
-				size++;
-				str++;
-			}
-		}
-		if (*str)
-			str++;
-	}
-	return (size);
-}
-
-// Returns the length of the variable values.
-size_t	get_value_length(char *var_name, t_env_list *env)
-{
-	size_t	var_size;
-	char	*str;
-	size_t	len;
-
-	var_size = ft_strlen(var_name);
-	len = 0;
-	while (env->content)
-	{
-		str = env->content;
-		if(ft_strnstr(str, var_name, var_size) && *(str + 4) == '=')
-		{
-			while (*(str - 1) != '=')
-				str++;
-			while (*str)
-			{
-				len++;
-				str++;
-			}
-		}
-		env = env->next;
-	}
-	return(len);
-}
-
-// Parameter: a pointer to the "$" char.
-// Return: an allocated string of the variable name, without the $.
-char	*get_var_name(char *str)
-{
-	char	*begin;
-	int		i;
-	size_t	len;
-
-	i = 1;
-	begin = &str[i];
-	len = 0;
-	while (!ft_isspace(str[i]) && !ft_isquote(str[i]) && str[i])
-	{
-		len++;
-		i++;
-	}
-	return(ft_substr(begin, 0, len));
-}
-
-// Matches each variable name with their value and returns the sum of the values
-// length. Nonexistent variables count as 0.
-size_t	get_values_size(char *str, t_env_list *env)
-{
-	char	*var_name;
-	size_t	size;
-
-	size = 0;
-	while (*str)
-	{
-		if (*str == '$')
-		{
-			var_name = get_var_name(str);
-			size += get_value_length(var_name, env);
-			free(var_name);
-		}
+		if (*str == '$' && is_variable(str[1]))
+			return (str);
 		str++;
 	}
-	return (size);
+	return (NULL);
 }
 
-char	*get_var_value(char *var_name, t_env_list *env)
+char *update_token(char *str, char *var_head)
 {
-	size_t	var_size;
-	size_t	len;
-	char	*str;
-	char	*var_value;
+	// char	*value;
+	char	*part1;
+	size_t	part1_len;
 
-	var_size = ft_strlen(var_name);
-	var_value = NULL;
-	len = 0;
-	while (env->content)
-	{
-		str = env->content;
-		if(ft_strnstr(str, var_name, var_size) && *(str + var_size) == '=')
-		{
-			while (*(str - 1) != '=')
-				str++;
-			while(str[len])
-				len++;
-			var_value = ft_substr(str, 0, len);
-		}
-		env = env->next;
-	}
-	return (var_value);
+	part1_len = 0;
+	while (str[part1_len] != *var_head)
+		part1_len++;
+	part1 = ft_substr(str, 0, part1_len);
+	return();
 }
 
-size_t	copy_value(char *str, const char *value_name)
-{
-	int	i;
-
-	i = 0;
-	while (value_name[i])
-	{
-		str[i] = value_name[i];
-		i++;
-	}
-	return (i);
-}
-
-// void	init_expand_var(t_ms_data *ms, size_t final_size)
-// {
-// 	t_expand_var	expv;
-	
-// 	expv.old_value = ms->tokens->value;
-// 	token->value = (char *) malloc(sizeof(char) * final_size);
-// }
-
-void	update_tokens(t_tokens *token, size_t final_size, t_env_list *env)
-{
-	char	*old_value;
-	char	*var_name;
-	char	*value_name;
-	size_t	t;
-	size_t	o;
-
-	old_value = token->value;
-	token->value = (char *) malloc(sizeof(char) * final_size);
-	t = 0;
-	o = 0;
-	while (old_value[o])
-	{
-		while (old_value[o] && (old_value[o] != '$'))
-			token->value[t++] = old_value[o++];
-		if (old_value[o] == '$')
-		{
-			var_name = get_var_name(&old_value[o]);
-			value_name = get_var_value(var_name, env);
-			if (value_name != NULL)
-				t += copy_value(&token->value[t], value_name);
-			safe_free(var_name);
-			safe_free(value_name);
-			while (!ft_isspace(old_value[o]) && old_value[o])
-			{
-				if (ft_isquote(old_value[o]))
-					break ;
-				o++;
-			}
-		}
-	}
-	free(old_value);
-}
-
-/*
-** Expand variables;
-** NOTE: try recursion for variables inside variables.
-*/
+// Iterates through tokens and expands their variables.
 int	expand_variables(t_ms_data *ms)
 {
-	size_t	str_size;
-	size_t	names_size;
-	size_t	values_size;
-	size_t	final_size;
+	char	*var_head;
+	int i = 1;
 
 	while (ms->tokens)
 	{
@@ -203,13 +58,13 @@ int	expand_variables(t_ms_data *ms)
 			ms->tokens = ms->tokens->next;
 			continue ;
 		}
-		str_size = ft_strlen(ms->tokens->value);
-		names_size = get_names_size(ms->tokens->value);
-		values_size = get_values_size(ms->tokens->value, ms->env_head);
-		final_size = str_size - names_size + values_size + 1;
-		update_tokens(ms->tokens, final_size, ms->env_head);
-		test_expand_vars(ms->tokens->value, str_size, names_size, values_size);
-		ms->tokens = ms->tokens->next;
+		var_head = find_variable(ms->tokens->value);
+		if (i--)
+		{
+			ms->tokens->value = update_token(ms->tokens->value, var_head);
+		}
+		else
+			ms->tokens = ms->tokens->next;
 	}
 	return (ms->state);
 }
---
+++ src/states/expand_vars_old.c
@@ -0,0 +1,207 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_vars.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viferrei <viferrei@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 17:40:14 by viferrei          #+#    #+#             */
+/*   Updated: 2022/09/08 00:16:32 by viferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+
+// Iterates through string and sums the size of all variables started by "$"
+size_t	get_names_size(char *str)
+{
+	size_t	size;
+
+	size = 0;
+	while (*str)
+	{
+		if (*str == '$')
+		{
+			size++;
+			str++;
+			while (!ft_isspace(*str) && !ft_isquote(*str) && *str)
+			{
+				size++;
+				str++;
+			}
+		}
+		if (*str)
+			str++;
+	}
+	return (size);
+}
+
+// Returns the length of the variable values.
+size_t	get_value_length(char *var_name, t_env_list *env)
+{
+	size_t	var_size;
+	char	*str;
+	size_t	len;
+
+	var_size = ft_strlen(var_name);
+	len = 0;
+	while (env->content)
+	{
+		str = env->content;
+		if(ft_strnstr(str, var_name, var_size) && *(str + 4) == '=')
+		{
+			while (*(str - 1) != '=')
+				str++;
+			while (*str)
+			{
+				len++;
+				str++;
+			}
+		}
+		env = env->next;
+	}
+	return(len);
+}
+
+// Parameter: a pointer to the "$" char.
+// Return: an allocated string of the variable name, without the $.
+char	*get_var_name(char *str)
+{
+	char	*begin;
+	int		i;
+	size_t	len;
+
+	i = 1;
+	begin = &str[i];
+	len = 0;
+	while (!ft_isspace(str[i]) && !ft_isquote(str[i]) && str[i])
+	{
+		len++;
+		i++;
+	}
+	return(ft_substr(begin, 0, len));
+}
+
+// Matches each variable name with their value and returns the sum of the values
+// length. Nonexistent variables count as 0.
+size_t	get_values_size(char *str, t_env_list *env)
+{
+	char	*var_name;
+	size_t	size;
+
+	size = 0;
+	while (*str)
+	{
+		if (*str == '$')
+		{
+			var_name = get_var_name(str);
+			size += get_value_length(var_name, env);
+			free(var_name);
+		}
+		str++;
+	}
+	return (size);
+}
+
+char	*get_var_value(char *var_name, t_env_list *env)
+{
+	size_t	var_size;
+	size_t	len;
+	char	*str;
+	char	*var_value;
+
+	var_size = ft_strlen(var_name);
+	var_value = NULL;
+	len = 0;
+	while (env->content)
+	{
+		str = env->content;
+		if(ft_strnstr(str, var_name, var_size) && *(str + var_size) == '=')
+		{
+			while (*(str - 1) != '=')
+				str++;
+			while(str[len])
+				len++;
+			var_value = ft_substr(str, 0, len);
+		}
+		env = env->next;
+	}
+	return (var_value);
+}
+
+size_t	copy_value(char *str, const char *value_name)
+{
+	int	i;
+
+	i = 0;
+	while (value_name[i])
+	{
+		str[i] = value_name[i];
+		i++;
+	}
+	return (i);
+}
+
+void	update_tokens(t_ms_data *ms, size_t final_size)
+{
+	size_t	t;
+	size_t	o;
+
+	t = 0;
+	o = 0;
+	ms->expv->old = ms->tokens->value;
+	ms->tokens->value = (char *) malloc(sizeof(char) * final_size);
+	while (ms->expv->old[o])
+	{
+		while (ms->expv->old[o] && (ms->expv->old[o] != '$'))
+			ms->tokens->value[t++] = ms->expv->old[o++];
+		if (ms->expv->old[o] == '$')
+		{
+			ms->expv->name = get_var_name(&ms->expv->old[o]);
+			ms->expv->value = get_var_value(ms->expv->name, ms->env_head);
+			if (ms->expv->value != NULL)
+				t += copy_value(&ms->tokens->value[t], ms->expv->value);
+			safe_free(ms->expv->name);
+			safe_free(ms->expv->value);
+			while (!ft_isspace(ms->expv->old[o]) && ms->expv->old[o])
+			{
+				if (ft_isquote(ms->expv->old[o]))
+					break ;
+				o++;
+			}
+		}
+	}
+	free(ms->expv->old);
+}
+
+/*
+** Expand variables;
+** NOTE: try recursion for variables inside variables.
+*/
+int	expand_variables(t_ms_data *ms)
+{
+	// size_t	str_size;
+	// size_t	names_size;
+	// size_t	values_size;
+	// size_t	final_size;
+
+	while (ms->tokens)
+	{
+		if (ms->tokens->value[0] == '\'')
+		{
+			ms->tokens = ms->tokens->next;
+			continue ;
+		}
+		// str_size = ft_strlen(ms->tokens->value);
+		// names_size = get_names_size(ms->tokens->value);
+		// values_size = get_values_size(ms->tokens->value, ms->env_head);
+		// final_size = str_size - names_size + values_size + 1;
+		// update_tokens(ms, final_size);
+		// test_expand_vars(ms->tokens->value, str_size, names_size, values_size);
+
+		
+
+		ms->tokens = ms->tokens->next;
+	}
+	return (ms->state);
+}
