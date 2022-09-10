55d8b0b17f58cd42336855f16f83aabe7ed85d97
Mochida <mochida@42>
2022-09-10 17:16:08 +0200

    chore(organization): moved some functions that'd be removed before evaluation to an easier place to clean up

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 23:09:16 by coder             #+#    #+#             */
-/*   Updated: 2022/09/10 16:47:38 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/10 17:14:25 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,16 @@ int			clean_state(t_ms_data *ms);
 void		*safe_free(void *content);
 void		*free_rl_split(t_ms_data *ms);
 void		*destroy_token_list(t_ms_data *ms);
-
 // ft_strcmp.c
 int			ft_strcmp (char *str, char *str2);
-
 // get_envs.c
 t_env_list	*get_env(char **envp);
 void		*destroy_env_list(t_env_list *envs);
+// test_utils.c
+int	print_token_list(t_ms_data *ms); // REMOVER ANTES DA ENTREGA
+int	check_for_some_shady_shit(t_ms_data *ms); // REMOVER ANTES DA ENTREGA
+
+
 
 /*
 ** PROMPT
--- src/states/exec_state.c
+++ src/states/exec_state.c
@@ -6,65 +6,11 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 20:59:48 by coder             #+#    #+#             */
-/*   Updated: 2022/09/10 01:21:53 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/10 17:14:32 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
-
-/*
-** THIS IS A DEBUGGING FUNCTION
-** prints the tokens. Yup. With printf.
-*/
-int	print_token_list(t_ms_data *ms)
-{
-	int	ret;
-	t_tokens *temp;
-
-	ret = 0;
-	temp = ms->tokens;
-	while (temp)
-	{
-		printf ("<: %p\n", temp->prev);
-		printf ("v: %p\n", temp);
-		printf (">: %p\n", temp->next);
-		ret += printf ("v: %s\n", temp->value);
-		printf ("t: %d\n", temp->type);
-		printf ("\n------------------\n");
-		temp = temp->next;
-	}
-	// printf ("total printed: %d\n", ret);
-	// sleep(5);
-	return (ret);
-}
-
-/*
-** THIS IS A DEBUGGING FUNCTION
-** sets state to state to -1 and returns the number of fucked up shit you've got
-** if all goes well, it'll continue to execute
-*/
-int	check_for_some_shady_shit(t_ms_data *ms)
-{
-	int	shaddy_shit_has_happened;
-
-	shaddy_shit_has_happened = 0;
-	if (!ms->tokens)
-	{
-		ms->state = -1;
-		shaddy_shit_has_happened++;
-	}
-	if (!ms->rl_prompt)
-	{
-		ms->state = -1;
-		shaddy_shit_has_happened++;
-	}
-	if (!ms->env_head)
-	{
-		ms->state = -1;
-		shaddy_shit_has_happened++;
-	}
-	return (shaddy_shit_has_happened);
-}
 
 /*
 ** executes the parsed and categorized commands
