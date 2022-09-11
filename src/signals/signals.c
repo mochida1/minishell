b11c6a9d281ac7968011899d3b2afca372b2abcf
Mochida <mochida@42>
2022-09-11 18:11:57 +0200

    feat(signals): ctrl+c now propperly working

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 20:29:26 by coder             #+#    #+#             */
-/*   Updated: 2022/09/10 23:16:48 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/11 18:11:05 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@
 */
 void	sigint_handler(int signo)
 {
-	printf ("%d MANDEI CTRL+C!\n", signo);
+	(void)signo;
+
+	write (1, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+	// printf ("%d MANDEI CTRL+C!\n", signo);
 }
 
 /*
@@ -25,6 +31,7 @@ void	sigint_handler(int signo)
 */
 void	sigterm_handler(int signo)
 {
+
 	printf ("%d, MANDEI CTRL+D!\n", signo);
 }
 
