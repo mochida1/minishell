4dcf09ddde5bbdfb293d4f7a70bf8e38c1e5592b
Mochida <mochida@42>
2022-09-17 17:50:18 +0200

    chore(header): now removed unused signal handlers

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 23:09:16 by coder             #+#    #+#             */
-/*   Updated: 2022/09/11 20:59:46 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/17 17:49:51 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,5 @@ int			token_is_command(t_tokens *temp);
 */
 // signals.c
 void		sigint_handler(int signo);
-void		sigterm_handler(int signo);
-void		sigquit_handler(int signo);
 
 #endif
