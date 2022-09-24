711ce59674887df69cd97b566aede3f5c5cb2408
viferrei <vifm@outlook.com>
2022-09-24 22:23:19 +0200

    fix(handle_redirects): check for existing infile and outfile command

--- src/exec/handle_redirects.c
+++ src/exec/handle_redirects.c
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 21:48:16 by viferrei          #+#    #+#             */
-/*   Updated: 2022/09/20 23:19:56 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/09/24 21:33:26 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@ void	handle_redirects(t_ms_data *ms)
 			outfile = tokens->next->value;
 		tokens = tokens->next;
 	}
-	if (access(infile, F_OK))
+	if (infile && access(infile, F_OK))
 		printf("%s: No such file or directory\n", infile);
-	else
+	else if (infile)
 		dup2(open(infile, O_RDONLY), STDIN_FILENO);
 	if (outfile)
 		dup2(open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0777), STDOUT_FILENO);
