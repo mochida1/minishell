da10484f4849c0256a66eb841814b6c5de967d6c
viferrei <viferrei@student.42sp.org.br>
2022-10-11 17:44:23 -0300

    correct infinite loop with pipe commands. Handle_pipes still not working

--- src/exec/exec_multi_cmd.c
+++ src/exec/exec_multi_cmd.c
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 23:19:44 by viferrei          #+#    #+#             */
-/*   Updated: 2022/10/10 02:27:10 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/10/11 17:42:55 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int	handle_pipes(t_com *cmd, int original_fds[2])
 {
-	static int	pipe_fds[2];
-	
+	int	pipe_fds[2];
+
 	if (cmd->receives_from_pipe)
 		dup2(pipe_fds[0], STDIN_FILENO);
 	if (cmd->sends_to_pipe)
 	{
-		pipe(pipe_fds);
+		if (pipe(pipe_fds) == -1)
+			printf("pipe error \n");
 		dup2(pipe_fds[1], STDOUT_FILENO);
 		close(pipe_fds[1]);
 	}
@@ -36,6 +37,10 @@ int	handle_pipes(t_com *cmd, int original_fds[2])
 int	exec_multi(t_com *cmd, t_ms_data *ms, int original_fds[2])
 {
 	int	control;
+
+	// control = 0;
+	// while (control++ < 1)
+	// 	exec_one_cmd(cmd, ms, original_fds);
 
 	control = handle_pipes(cmd, original_fds);
 	exec_one_cmd(cmd, ms, original_fds);
