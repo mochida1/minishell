1692d9f3733ff9c6f8e414d7ef72d5890e882a04
viferrei <viferrei@student.42sp.org.br>
2022-10-12 14:44:45 -0300

    feat(pipe) pipes working git add .git add .!

--- src/exec/exec_multi_cmd.c
+++ src/exec/exec_multi_cmd.c
@@ -6,18 +6,21 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 23:19:44 by viferrei          #+#    #+#             */
-/*   Updated: 2022/10/11 17:42:55 by viferrei         ###   ########.fr       */
+/*   Updated: 2022/10/12 14:43:16 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-int	handle_pipes(t_com *cmd, int original_fds[2])
+int	handle_pipes(t_com *cmd, int original_output)
 {
-	int	pipe_fds[2];
+	static int	pipe_fds[2];
 
 	if (cmd->receives_from_pipe)
+	{
 		dup2(pipe_fds[0], STDIN_FILENO);
+		close(pipe_fds[0]);
+	}
 	if (cmd->sends_to_pipe)
 	{
 		if (pipe(pipe_fds) == -1)
@@ -27,8 +30,8 @@ int	handle_pipes(t_com *cmd, int original_fds[2])
 	}
 	else
 	{
-		dup2(original_fds[1], STDOUT_FILENO);
-		close(pipe_fds[1]);
+		dup2(original_output, STDOUT_FILENO);
+		close(original_output);
 		return (0);
 	}
 	return (1);
@@ -38,11 +41,12 @@ int	exec_multi(t_com *cmd, t_ms_data *ms, int original_fds[2])
 {
 	int	control;
 
-	// control = 0;
-	// while (control++ < 1)
-	// 	exec_one_cmd(cmd, ms, original_fds);
+	if (original_fds[0] == NO_REDIRECT)
+		original_fds[0] = dup(STDIN_FILENO);
+	if (original_fds[1] == NO_REDIRECT)
+		original_fds[1] = dup(STDOUT_FILENO);
 
-	control = handle_pipes(cmd, original_fds);
+	control = handle_pipes(cmd, original_fds[1]);
 	exec_one_cmd(cmd, ms, original_fds);
 	return (control);
 }
